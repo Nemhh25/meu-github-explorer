04564d333eac6dd69883e399d1cd3a8e5027a226
Nemhh25 <nelsondossantos739@gmail.com>
2026-08-15 21:38:02 -0300

    files updated

---
+++ src/components/RepoCard.jsx
@@ -0,0 +1,17 @@
+function RepoCard({ repo }) {
+    return (
+        <li>
+            <h3>{repo.name}</h3>
+
+            {repo.description && <p>{repo.description}</p>}
+
+            <p>Linguagem: {repo.language}</p>
+
+            <p>⭐ {repo.stargazers_count}</p>
+
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">Ver no GitHub</a>
+        </li>
+    )
+}
+
+export default RepoCard
